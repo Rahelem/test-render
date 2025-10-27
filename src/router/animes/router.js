52e8b54cf8a9979c0406c41c7b84bfa017f32793
Raphael <raphael.lesmann@dci-student.org>
2025-10-27 10:15:11 +0100

    add middleware , add router put

--- src/router/animes/router.js
+++ src/router/animes/router.js
@@ -5,12 +5,38 @@ import animes from './animes.js';
 const router = express.Router();
 
 
+function validateAnimeData(req, res, next) {
+    const {title, genre} = req.body;
+    if(!title || !genre) {
+        res.status(400).json({ error: "Missig title and/or genre"});
+        return
+    }
+
+    if (typeof title !== "string" || typeof genre !== "string") {
+        res.status(400).json({ error: "Title and genre must be a string"});
+        return
+    }
+    next()
+}
+
+function validateAnimeId(req, res, next) {
+    const id = Number(req.params.id);
+    const found = animes.find(a => a.id === id);
+    if (!found) {
+        res.status(404).json({ error: "Anime not found" });
+        return
+    }
+    next();
+};
+
 
 router.get('/', (req, res) => {
     res.status(200).json(animes);
 });
 
-router.get('/:id', (req, res) => {
+
+
+router.get('/:id', validateAnimeId, (req, res) => {
     const id = parseInt(req.params.id);
     const anime = animes.find(a => a.id === id);
 
@@ -21,42 +47,43 @@ router.get('/:id', (req, res) => {
     res.json(router);
 });
 
-router.post('/', (req, res) => {
-    const {id, title, genre } = req.body;
-    if ( !id | !title | !genre) {
-        res.status(404).json({ message: "Missing fields"})
-    }
+router.post('/', validateAnimeData, (req, res) => {
+    const { title, genre } = req.body;
+   
 
-    const newAnime = {id, title, genre};
+    const newAnime = { id: animes.length + 1, title, genre };
     animes.push(newAnime);
     res.status(201).json(newAnime)
 });
 
-router.patch('/:id', (req, res) => {
+router.patch('/:id', validateAnimeId, (req, res) => {
     const id = parseInt(req.params.id);
     const anime = anime.find(a => a.id === id);
 
-    if(!anime) {
-        res.status(404).json({ error: "Anime not found"});
-    }
+    const { title, genre } = req.body;
+    if (title) anime.title = title;
+    if (genre) anime.genre = genre;
 
-     const { title, genre } = req.body;
-  if (title) anime.title = title;
-  if (genre) anime.genre = genre;
-
-  res.status(200).json(anime);
+    res.status(200).json(anime);
 });
 
-router.delete('/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = animes.findIndex(a => a.id === id);
+router.put('/:id', validateAnimeId, validateAnimeData, (req, res) => {
+    const id = parseInt(req.params.id);
+    const anime = animes.find(a => a.id  === id);
 
-  if (index === -1) {
-    return res.status(404).json({ error: 'Anime not found' });
-  }
+    const { title, genre } = req.body;
+    anime.title = title; 
+    anime.genre = genre; 
 
-  const deleted = animes.splice(index, 1);
-  res.status(200).json({ deleted });
+    res.status(200).json(anime);
+});
+
+router.delete('/:id', validateAnimeId, (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = animes.findIndex(a => a.id === id);
+    const deleted = animes.splice(index, 1);
+  
+    res.status(200).json({ deleted });
 });
 
 
